05607832819fad84d5908d793deff5815644faf7
Cindy Liang <cindywritescode@gmail.com>
2023-02-05 18:08:20 +0000

    use yarn to install TensorFlow post detection model. Create front-end with Next.js to show the points.

---
+++ tf/evaluate_movenet.js
@@ -0,0 +1,16 @@
+import fs from 'fs';
+import * as poseDetection from '@tensorflow-models/pose-detection';
+import * as tfn from '@tensorflow/tfjs-node';
+import '@tensorflow/tfjs-backend-wasm';
+
+const detectorConfig = {
+  modelType: poseDetection.movenet.modelType.MULTIPOSE_LIGHTNING,
+  enableTracking: true,
+  trackerType: poseDetection.TrackerType.BoundingBox
+};
+const detector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet, detectorConfig);
+
+const buffer = fs.readFileSync('../public/tree00000000.jpg');
+const image = tfn.node.decodeJpeg(buffer);
+const poses = await detector.estimatePoses(image);
+console.log(JSON.stringify(poses, null, 2));
